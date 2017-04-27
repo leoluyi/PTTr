6ac295b793892cdee30eee4b9aad8cc0e4b93867
leoluyi <leo0650@gmail.com>
2017-04-27 20:28:14 +0800

    Enable list of push in dataframe

--- R/get_all_posts.R
+++ R/get_all_posts.R
@@ -13,19 +13,23 @@
 #' @examples
 #' get_all_posts("Gossiping", max_post = 10)
 #'
-get_all_posts <- function(board_name, max_post = 1000, include_push = FALSE,
-                          mc.cores = -1, ...) {
+get_all_posts <- function(board_name, max_post = 1000, list_push = FALSE,
+                          parallel = TRUE, mc.cores = NULL, ...) {
   # board_name = "Gossiping"
-  # max_post = 100
-  # mc.cores = 1
+  # max_post = 20
+  # list_push = TRUE
+  # parallel = TRUE
+  # mc.cores = NULL
   # post_urls = "https://www.ptt.cc/bbs/Gossiping/M.1468224573.A.D15.html"
 
   ## Setting # of parallel cores
-  if (mc.cores == -1L) {
-    mc.cores <- parallel::detectCores()-1
+  if (!parallel) {
+    mc.cores <- 1L
+  } else if (is.null(mc.cores)) {
+    mc.cores <- ceiling(parallel::detectCores() * 3/4)
   } else if (!is.numeric(mc.cores) || mc.cores < 1) {
     mc.cores <- 1L
-  } else {
+  } else if (is.numeric(mc.cores)) {
     mc.cores <- as.integer(mc.cores)
   }
 
@@ -39,27 +43,36 @@ get_all_posts <- function(board_name, max_post = 1000, include_push = FALSE,
   on.exit(stopCluster(cl))
   clusterExport(cl, c("post_urls"), envir = environment())
   clusterExport(cl, "get_post_content", envir = as.environment("package:PTTr"))
-  clusterEvalQ(cl, library(data.table))
+  invisible(clusterEvalQ(cl, library(data.table)))
+
+  try_get_post_content <- function(x) {
+    tryCatch({
+      post_data <- get_post_content(x, verbose = FALSE)
+
+      if (!is.null(post_data$post_main)) {
+        out <- data.table::as.data.table(post_data$post_main)
+
+        if (list_push) {
+          out[, push_data := list(list(post_data$push))]
+        } else {
+          out$push_text <- post_data$push$push_text %>% paste(collapse=" ")
+        }
+      } else {
+        out <- NULL
+      }
+      out
+    }, error = function(e) {
+      message(e, "[url] ",x)
+    }, warning = function(w) {
+      message(w, "[url] ",x)
+    })
+  }
 
   res_list <- pbapply::pblapply(
     cl=cl,
     post_urls,
-    function(x) {
-      tryCatch({
-        post_data <- get_post_content(x, verbose = FALSE)
-        if (!is.null(post_data$post_main)) {
-          out <- data.table::as.data.table(post_data$post_main)
-          out$push_text <- post_data$push$push_text %>% paste(collapse=" ")
-        } else {
-          out <- NULL
-        }
-        out
-      }, error = function(e) {
-        message(e, "[url] ",x)
-      }, warning = function(w) {
-        message(w, "[url] ",x)
-      })
-    })
+    try_get_post_content
+  )
 
   post_dt <- data.table::rbindlist(res_list, use.names = TRUE, fill = TRUE)
   post_dt
