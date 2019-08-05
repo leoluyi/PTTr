ce1a7195f7713b509af8567ad97b7157d3e3f497
Leo Lu <leoluyi@users.noreply.github.com>
2019-08-05 13:34:35 +0800

    Update PTTr-package.R

--- R/PTTr-package.R
+++ R/PTTr-package.R
@@ -6,7 +6,6 @@
 #' @docType package
 #' @import magrittr httr rvest stringr parallel
 #' @importFrom utils head tail
-#' @importFrom dplyr bind_rows data_frame
 #' @importFrom data.table data.table rbindlist
 #' @importFrom xml2 read_html
 NULL
